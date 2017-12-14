1fdeb1013c2ce847035f0c1a170c12274983843a
Darren Eggenschwiler <deggen@Darrens-MacBook-Pro.local>
2017-12-14 23:39:55 +0000

    init

--- ScroogeCoin/src/TxHandler.java
+++ ScroogeCoin/src/TxHandler.java
@@ -1,28 +1,41 @@
+import java.util.ArrayList;
+
 public class TxHandler {
+    UTXOPool uPool;
+    public TxHandler(UTXOPool utxoPool) {
+        UTXOPool uPool = new UTXOPool(utxoPool);
+    }
 
-    /** Creates a public ledger whose current UTXOPool
-     * (collection of unspent transaction outputs) is utxoPool.
-     * This should  make a defensive copy of utxoPool by using
-     * the UTXOPool (UTXOPool uPool) constructor.
+    /**
+     * @return true if:
+     * (1) all outputs claimed by {@code tx} are in the current UTXO pool, 
+     * (2) the signatures on each input of {@code tx} are valid, 
+     * (3) no UTXO is claimed multiple times by {@code tx},
+     * (4) all of {@code tx}s output values are non-negative, and
+     * (5) the sum of {@code tx}s input values is greater than or equal to the sum of its output
+     *     values; and false otherwise.
      */
-    public TxHandler(UTXOPool utxoPool);
+    public boolean isValidTx(Transaction tx) {
+        //get the inputs
+        ArrayList<Transaction.Input> allinputs = tx.getInputs();
 
-    /** Returns true if
-     * (1) all outputs claimed by tx are in the current UTXO pool
-     * (2) the signatures on each input of tx are valid
-     * (3) no UTXO is claimed multiple times by tx
-     * (4) all of tx’s output values are non-negative
-     * (5) the sum of tx’s input values is greater than or equal
-     * to the sum of its output values; and false otherwise.
-     */
-    public boolean isValidTx(Transaction tx);
+        //for each input, check the outout is in the utxo
+        for (Transaction.Input i: allinputs) {
+            UTXO outputToCheck = new UTXO(i.prevTxHash, i.outputIndex);
+            uPool.contains(outputToCheck);
+        }
 
-    /** Handles each epoch by receiving an unordered array of
-     * proposed transactions, checking each transaction for
-     * correctness, returning a mutually valid array of accepted
-     * transactions, and updating the current UTXO pool as
-     * appropriate.
+
+
+    }
+
+    /**
+     * Handles each epoch by receiving an unordered array of proposed transactions, checking each
+     * transaction for correctness, returning a mutually valid array of accepted transactions, and
+     * updating the current UTXO pool as appropriate.
      */
-    public Transaction[] handleTxs(Transaction[] possibleTxs);
+    public Transaction[] handleTxs(Transaction[] possibleTxs) {
+        // IMPLEMENT THIS
+    }
 
 }
